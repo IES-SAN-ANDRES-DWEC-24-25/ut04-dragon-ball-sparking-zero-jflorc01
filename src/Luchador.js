f6c29fb4328667e5a2c30b9ce91b829f256ee01e
Juan Álvaro Flórez <jflorc01@estudiantes.unileon.es>
2024-11-05 02:07:07 +0100

    Luchador y simularBatalla

--- src/Luchador.js
+++ src/Luchador.js
@@ -1,104 +1,76 @@
 // src/Luchador.js
 
 class Luchador {
-    // Implementa el constructor y los métodos necesarios
-    
-    constructor(nombre, velocidad, ataque, defensa, salud) {
+  // Implementa el constructor y los métodos necesarios
 
-      this.nombre = nombre;
-      this.velocidad = velocidad;
-      this.ataque = ataque;
-      this.defensa = defensa;
-      this.salud = salud;
-      
-    }
-    
-    /**
-     * Realiza un ataque contra un oponente.
-     * @param {Luchador} oponente - El luchador que recibe el ataque.
-     * @returns {Object} - Resultado del ataque.
-     */
-    atacar(oponente) {
-      // Decidir si el ataque es esquivado
-
-      let aleat = Math.floor(Math.random() * 100) + 1;
-      let esquiva;
-      let ataqRealizado = false;
-
-      if(this.velocidad >= oponente.velocidad && aleat > 25){
-
-        esquiva = false; 
-
-      }else if(this.velocidad < oponente.velocidad && aleat > 75){
-
-        esquiva = false;
-
-      }else{
-
-        esquiva = true;
-      
-      }
-      // Calcular daño
-      
-      if(!esquiva){
-
-        oponente.recibirDanio(this.ataque);
-        ataqRealizado = true;
-
-      }
-
-      if(ataqRealizado){
-
-        let result;
-
-        if(oponente.estaVivo() - ){
-
-          result = this.nombre + "ha atacado ";
-        }
-
-      }
-
-      // Asegurar que el daño no sea negativo
-      
-  
-      // Aplicar daño al oponente
-     
-  
-      // Retornar resultado del ataque
-      
-    }
-  
-    /**
-     * Aplica daño a la salud del luchador.
-     * @param {number} danio - Cantidad de daño recibido.
-     */
-    recibirDanio(danio) {
-
-      // Si tiene más defensa, el ataque será muy pequeño
-
-      if(this.defensa > danio){
-
-        this.salud -= (danio * 0.2);
-
-      }else{
-
-        let aux = danio - this.defensa;
-        this.salud -= aux;
-
-      }
-
-    }
-  
-    /**
-     * Verifica si el luchador está vivo.
-     * @returns {boolean} - `true` si la salud es mayor a 0, `false` en caso contrario.
-     */
-    estaVivo() {
-      
-      return this.salud > 0;
-
-    }
+  constructor(nombre, velocidad, ataque, defensa, salud) {
+    this.nombre = nombre;
+    this.velocidad = velocidad;
+    this.ataque = ataque;
+    this.defensa = defensa;
+    this.salud = salud;
   }
-  
-  module.exports = Luchador;
-  
+
+  /**
+   * Realiza un ataque contra un oponente.
+   * @param {Luchador} oponente - El luchador que recibe el ataque.
+   * @returns {Object} - Resultado del ataque.
+   */
+  atacar(oponente) {
+    // Decidir si el ataque es esquivado
+    // En este caso, la probabilidad de esquivar el ataque es del 20%
+    
+    // !: Qué devuelve el método?
+
+    let danio;
+    
+    let rand = Math.random();
+    if(rand < 0.2){
+      
+      console.log(`${oponente.nombre} ha esquivado el ataque!`);
+      danio = 0;
+
+    }
+
+    // Calcular daño
+    // Asegurar que el daño no sea negativo
+
+    if(oponente.defensa > this.ataque){
+    
+      danio = 0.2 * this.ataque;
+    
+    }else{
+      
+      danio = this.ataque - oponente.defensa;
+
+    }
+
+    // Aplicar daño al oponente
+
+    oponente.recibirDanio(danio);
+
+    // Retornar resultado del ataque
+  }
+
+  /**
+   * Aplica daño a la salud del luchador.
+   * @param {number} danio - Cantidad de daño recibido.
+   */
+  recibirDanio(danio) {
+    
+    this.salud -= danio;
+    console.log(`${this.nombre} ha recibido ${danio} puntos de daño`);
+    console.log(`Salud restante de ${this.nombre}: ${this.salud}`);
+    
+  }
+
+  /**
+   * Verifica si el luchador está vivo.
+   * @returns {boolean} - `true` si la salud es mayor a 0, `false` en caso contrario.
+   */
+  estaVivo() {
+    return this.salud > 0;
+  }
+}
+
+module.exports = Luchador;
--- src/simularBatalla.js
+++ src/simularBatalla.js
@@ -8,20 +8,61 @@
  */
 function simularBatalla(luchador1, luchador2) {
     console.log(`\nComienza la batalla entre ${luchador1.nombre} y ${luchador2.nombre}!`);
-  
     
-  
     // Determinar quién ataca primero basado en la velocidad
-    
-      // Si la velocidad es igual, elegir al azar
+    // Si la velocidad es igual, elegir al azar
+    let atacante;
+    let defensor;
+
+    if(luchador1.velocidad == luchador2.velocidad){
       
-  
-    console.log(`${atacante.nombre} tiene mayor velocidad y ataca primero.`);
+      let rand = Math.random();
+
+      if(rand < 0.5){
+        
+        atacante = luchador1;
+        defensor = luchador2;
+
+      }else{
+
+        atacante = luchador2;
+        defensor = luchador1;
+
+      }
+    }else if(luchador1.velocidad > luchador2.velocidad){
+
+      atacante = luchador1;
+      defensor = luchador2;
+
+    }else{
+
+      atacante = luchador2;
+      defensor = luchador1;
+
+    }
   
     // Simular turnos hasta que uno de los luchadores pierda
    
+    while(atacante.estaVivo() && defensor.estaVivo()){
+
+      console.log(`${atacante.nombre} tiene mayor velocidad y ataca primero.`);
+      atacante.atacar(defensor);
       
-    const ganador = null;
+      if(defensor.estaVivo()){
+        console.log(`Es el turno de ${defensor.nombre}.`);
+        defensor.atacar(atacante);
+      }else{
+        console.log(`${defensor.nombre} se ha debilitado.`);
+      }
+
+    }
+    
+    if(atacante.estaVivo()){
+      const ganador = atacante;
+    }else{
+      const ganador = defensor;
+    }
+  
     console.log(`El ganador de la batalla es ${ganador.nombre}!\n`);
     return ganador;
   }
